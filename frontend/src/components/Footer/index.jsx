e46dc3e33aaddac5ba04712a93a730b9e23314c4
cingihimut <cingihimutbanget@gmail.com>
2023-12-10 17:14:18 +0700

    fixing error

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 const Footer = () => {
     return (
         <>
-            <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+            <footer className="bg-white rounded-lg shadow dark:bg-gray-900">
                 <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                     <div className="sm:flex sm:items-center sm:justify-between">
                         <Link href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
