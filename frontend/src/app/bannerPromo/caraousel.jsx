d03c1138372929ba3c229c7a75033a8e22a9f073
Muhamamad Aris <mhdarisid@gmail.com>
2023-12-12 11:21:14 +0700

    Fix Caroausel

---
+++ frontend/src/app/bannerPromo/caraousel.jsx
@@ -0,0 +1,53 @@
+"use client";
+import { ArrowCircleLeft, ArrowCircleRight } from "@phosphor-icons/react";
+import Image from "next/image";
+import { useState } from "react";
+
+const Carousel = () => {
+  const images = [
+    "/assets/Banner-1.png",
+    "/assets/Banner-2.png",
+    "/assets/Banner-3.png",
+    "/assets/Banner-4.png",
+  ];
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const nextImage = () => {
+    setCurrentImage((prev) => (prev + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentImage((prev) => (prev - 1 + images.length) % images.length);
+  };
+
+  return (
+    <div className="relative p-4">
+      <Image
+        src={images[currentImage]}
+        alt={`Image ${currentImage + 1}`}
+        width={700}
+        height={300}
+        className="w-full h-64 object-cover rounded-l"
+        onError={(e) => console.error(`Error loading image: ${e.target.src}`)}
+      />
+
+      <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-between p-4">
+        <button
+          onClick={prevImage}
+          className="text-white bg-gray-800 px-4 py-2 rounded-full opacity-75 hover:opacity-100"
+        >
+          <ArrowCircleLeft size={20} />
+        </button>
+        <button
+          onClick={nextImage}
+          className="text-white bg-gray-800 px-4 py-2 rounded-full opacity-75 hover:opacity-100"
+        >
+          <ArrowCircleRight size={20} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,22 +1,21 @@
-import Footer from "@/components/Footer/index"
-import NavBar from "@/components/NavBar/index"
-import Kategori from "./kategori/page"
-import Carousel from "./bannerPromo/page"
+import Footer from "@/components/Footer/index";
+import NavBar from "@/components/NavBar/index";
+import Kategori from "./kategori/page";
+import Carousel from "./bannerPromo/caraousel";
 
 const Page = () => {
-
   return (
     <>
-      <NavBar/>
-        <main>
-          <div className="py-6">
-            <Carousel/>
-          </div>
-        </main>
-        <p className="font-bold p-4">Kategori</p>
-        <Kategori/>
-      <Footer/>
+      <NavBar />
+      <main>
+        <div className="py-6">
+          <Carousel />
+        </div>
+      </main>
+      <p className="font-bold p-4">Kategori</p>
+      <Kategori />
+      <Footer />
     </>
-  )
-}
-export default Page
+  );
+};
+export default Page;
